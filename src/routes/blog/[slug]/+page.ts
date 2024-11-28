0fab0259032935fc466f8a7ba8e2ff68b1f9cd4b
Chad Freeman <ceo@marinadev.xyz>
2024-11-28 03:58:24 -0500

    add metadata to sveltekit entries

--- src/routes/blog/[slug]/+page.ts
+++ src/routes/blog/[slug]/+page.ts
@@ -1,7 +1,10 @@
-import { pages } from '$lib/pages';
+import { metadata, pages } from '$lib/pages';
 
 export const prerender = true;
 
+export const entries = () => {
+    return metadata.map(v => ({ slug: v.name }))
+}
 export function load({ params: { slug } }) {
 	const page = pages[slug];
 	return {
